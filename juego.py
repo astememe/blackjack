d6d8a8c9739296ae756a7e6a5923830127279e7d
StoianAlejandro <s.alejandrostoian@gmail.com>
2025-03-05 11:22:07 +0100

    Creados más métodos

--- juego.py
+++ juego.py
@@ -8,10 +8,43 @@ baraja = []
 manoJugador = []
 manoCrupier = []
 
-def repartir(baraja):
-    for i in range (2):
-        manoJugador.append(baraja.pop())
-        manoCrupier.append(baraja.pop())
+def crearBaraja():
+    for i in range(4):
+        for j in range(10):
+            if j >= 7:
+                nombres[7] = "Sota"
+                nombres[8] = "Caballo"
+                nombres[9] = "Rey"
+            if i == 0:
+                cartita = Carta("Bastos", valores[j], nombres[j])
+                baraja.append(cartita)
+            elif i == 1:
+                cartita = Carta("Copas", valores[j], nombres[j])
+                baraja.append(cartita)
+            elif i == 2:
+                cartita = Carta("Oros", valores[j], nombres[j])
+                baraja.append(cartita)
+            else:
+                cartita = Carta("Espadas", valores[j], nombres[j])
+                baraja.append(cartita)
+
+
+def repartir(baraja, numero, mano):
+    for i in range (numero):
+        mano.append(baraja.pop())
+
+def mostrarCartas():
+    print(f"Cartas del jugador:")
+    for i in range(len(manoJugador)):
+        print(Carta.getCarta(manoJugador[i]))
+    print(f"Cartas del crupier:")
+
+
+def sumarCartas(mano):
+    suma = 0
+    for i in range(len(mano)):
+        suma += Carta.getValor(mano[i])
+    return suma
 
 def empezarJuego(manoJugador, manoCrupier, baraja):
     suma = 16
@@ -32,27 +65,7 @@ def empezarJuego(manoJugador, manoCrupier, baraja):
                 print(f"{manoJugador[j]}")
             print(f"Tienes suma: {suma.__str__()}")
 
-for i in range(4):
-    for j in range(10):
-        if j >= 7:
-            nombres[7] = "Sota"
-            nombres[8] = "Caballo"
-            nombres[9] = "Rey"
-        if i == 0:
-            cartita = Carta("Bastos", valores[j], nombres[j])
-            baraja.append(cartita)
-        elif i == 1:
-            cartita = Carta("Copas", valores[j], nombres[j])
-            baraja.append(cartita)
-        elif i == 2:
-            cartita = Carta("Oros", valores[j], nombres[j])
-            baraja.append(cartita)
-        else:
-            cartita = Carta("Espadas", valores[j], nombres[j])
-            baraja.append(cartita)
 
 shuffle(baraja)
 repartir(baraja)
 empezarJuego(manoJugador, manoCrupier, baraja)
-
-
